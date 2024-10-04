04c538a232420837ced14c8edee0a0af8297c961
Pavel Yafimau <p.yafimau@wolf-wolf.net>
2024-10-04 17:09:17 +0300

    AlineRevenueRMS.Automation.Web.Tests has been added

---
+++ AlineRevenueRMS.Automation.Web.Tests/Tests/Base/BaseLogin.cs
@@ -0,0 +1,27 @@
+﻿using AlineRevenueRMS.Automation.Web.Core.Configuration;
+using AlineRevenueRMS.Automation.Web.Tests.Pages;
+using NUnit.Framework;
+
+namespace AlineRevenueRMS.Automation.Web.Tests.Tests.Base
+{
+    public class BaseLogin : BaseInitWebDriver
+    {
+        private readonly int _setId;
+
+        public BaseLogin(int setId)
+        {
+            _setId = setId;
+        }
+
+        [SetUp]
+        public void SetupLogIn()
+        {
+            var userToLogin = AppConfiguration.Users.Single(u => u.SetId == _setId);
+
+            LoginPage.Open();
+            LoginPage.UserNameField.Set(userToLogin.UserName);
+            LoginPage.PasswordField.Set(userToLogin.Password);
+            LoginPage.SignInButton.Click();
+        }
+    }
+}
---
+++ AlineRevenueRMS.Automation.Web.Tests/Tests/SampleTest.cs
@@ -0,0 +1,20 @@
+﻿using AlineRevenueRMS.Automation.Web.Core.Conditions;
+using AlineRevenueRMS.Automation.Web.Tests.Pages;
+using AlineRevenueRMS.Automation.Web.Tests.Tests.Base;
+using NUnit.Framework;
+
+namespace AlineRevenueRMS.Automation.Web.Tests.Tests
+{
+    public class SampleTest : BaseLogin
+    {
+        public SampleTest() : base(1)
+        {
+        }
+
+        [Test]
+        public void TestObject_Action_ExpectedResult()
+        {
+            LoginPage.TenantCollectionValidateWarning.Should(Be.Visible);
+        }
+    }
+}
