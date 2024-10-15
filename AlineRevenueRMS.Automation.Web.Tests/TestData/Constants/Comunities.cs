d8a6a54dbffe2c8be57f3784a4d815740079de0f
Liliia Baieva <555blv@gmail.com>
2024-10-15 12:40:52 +0300

    Change the values of comunyties

--- AlineRevenueRMS.Automation.Web.Tests/TestData/Constants/Comunities.cs
+++ AlineRevenueRMS.Automation.Web.Tests/TestData/Constants/Comunities.cs
@@ -8,12 +8,11 @@ namespace AlineRevenueRMS.Automation.Web.Tests.TestData.Constants
 {
     public static class Comunities
     {
-        public const string WENTWORT_CENTRAL_AVENUE = "Wentworth Central Avenue (12006) ";
-        public const string ANCHOR_BAY_POCASSET = "Anchor Bay Pocasset (18003) ";
-        public const string ELEGANCE_AT_LAKE_WORTH = "Elegance at Lake Worth (12007) ";
-        public const string SYMPHONY_MANOR_ROLAND_PARK = "Symphony Manor Roland Park (14001) ";
-        public const string SYMPHONY_OLMSTED_FALLS = "Symphony Olmsted Falls (16003) ";
-        public const string TRANQUILLITY_FREDERICKTOWNE = "Tranquillity Fredericktowne (14002) ";
-        public const string DEMO_COMUNITY = "Demo Community (99999) ";
+        public const string WENTWORT_CENTRAL_AVENUE = "Wentworth Central Avenue";
+        public const string ANCHOR_BAY_POCASSET = "Anchor Bay Pocasset";
+        public const string ELEGANCE_AT_LAKE_WORTH = "Elegance at Lake Worth";
+        public const string SYMPHONY_MANOR_ROLAND_PARK = "Symphony Manor Roland Park";
+        public const string SYMPHONY_OLMSTED_FALLS = "Symphony Olmsted Falls";
+        public const string TRANQUILLITY_FREDERICKTOWNE = "Tranquillity Fredericktowne";
     }
 }
