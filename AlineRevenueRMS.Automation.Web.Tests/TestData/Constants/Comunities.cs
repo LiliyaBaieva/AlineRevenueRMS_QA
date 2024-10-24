fbaee2829e4209b027137494e490225fb90c2c27
Liliia Baieva <555blv@gmail.com>
2024-10-24 16:58:12 +0200

    Remove TestProject

--- AlineRevenueRMS.Automation.Web.Tests/TestData/Constants/Comunities.cs
+++ AlineRevenueRMS.Automation.Web.Tests/TestData/Constants/Comunities.cs
@@ -3,10 +3,10 @@
     public static class Comunities
     {
         public const string WENTWORT_CENTRAL_AVENUE = "Wentworth Central Avenue";
-        public const string ANCHOR_BAY_POCASSET = "Anchor Bay Pocasset";
+        public const string ANCHOR_BAY_POCASSET = "Anchor Bay at East Providence";
         public const string ELEGANCE_AT_LAKE_WORTH = "Elegance at Lake Worth";
-        public const string SYMPHONY_MANOR_ROLAND_PARK = "Symphony Manor Roland Park";
-        public const string SYMPHONY_OLMSTED_FALLS = "Symphony Olmsted Falls";
-        public const string TRANQUILLITY_FREDERICKTOWNE = "Tranquillity Fredericktowne";
+        public const string SYMPHONY_MANOR_ROLAND_PARK = "Symphony Manor at Roland Park";
+        public const string SYMPHONY_OLMSTED_FALLS = "Symphony Manor at Roland Park";
+        public const string TRANQUILLITY_FREDERICKTOWNE = "Tranquillity at Fredericktowne";
     }
 }
